1e57ff5339ae34491ad0607c7d9af3127aa38a0d
mryeminaung <yeminaung013@gmail.com>
2023-12-16 21:51:23 +0630

    sorting methods implementation

---
+++ 1D-Array/sortBy_descending_order.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+#define SIZE 100
+
+int main()
+{
+    int arr[SIZE];
+    int num = 5, temp = 0;
+
+    printf("Enter number of elements : ");
+    scanf("%d", &num);
+
+    printf("\nEnter entries : \n");
+
+    for (int i = 0; i < num; i++)
+        scanf("%d", &arr[i]);
+
+    printf("\nOriginal array : ");
+    for (int i = 0; i < num; i++)
+    {
+        if (i == 0)
+            printf("[");
+        else
+            printf(", ");
+
+        printf("%d", arr[i]);
+
+        if (i == num - 1)
+            printf("]\n");
+    }
+
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < num - 1; j++)
+        {
+            if (arr[j] < arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("\nSorting By Descending Order : ");
+    for (int i = 0; i < num; i++)
+    {
+        if (i == 0)
+            printf("[");
+        else
+            printf(", ");
+
+        printf("%d", arr[i]);
+
+        if (i == num - 1)
+            printf("]\n");
+    }
+
+    return 0;
+}
